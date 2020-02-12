420beed6b834844fd4c809624cd0e2eae9c8bc9b
SofiaFlores07 <s.vanessaf2001@gmail.com>
2020-02-12 13:34:28 -0600

    Practica1

---
+++ PortafolioSofiaFlores/practica1/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace practica1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.txtPromedio = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtExamen3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtExamen2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtExamen1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(240, 355);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(81, 32);
+            this.btnSalir.TabIndex = 21;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(139, 355);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(83, 34);
+            this.btnLimpiar.TabIndex = 20;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(21, 355);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(96, 34);
+            this.btnCalcular.TabIndex = 19;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // txtPromedio
+            // 
+            this.txtPromedio.Location = new System.Drawing.Point(156, 222);
+            this.txtPromedio.Name = "txtPromedio";
+            this.txtPromedio.ReadOnly = true;
+            this.txtPromedio.Size = new System.Drawing.Size(100, 26);
+            this.txtPromedio.TabIndex = 18;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(45, 229);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 20);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "Promedio";
+            // 
+            // txtExamen3
+            // 
+            this.txtExamen3.Location = new System.Drawing.Point(156, 146);
+            this.txtExamen3.Name = "txtExamen3";
+            this.txtExamen3.Size = new System.Drawing.Size(100, 26);
+            this.txtExamen3.TabIndex = 16;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(45, 153);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 20);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Examen3";
+            // 
+            // txtExamen2
+            // 
+            this.txtExamen2.Location = new System.Drawing.Point(156, 88);
+            this.txtExamen2.Name = "txtExamen2";
+            this.txtExamen2.Size = new System.Drawing.Size(100, 26);
+            this.txtExamen2.TabIndex = 14;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(41, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 20);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Examen2";
+            // 
+            // txtExamen1
+            // 
+            this.txtExamen1.Location = new System.Drawing.Point(156, 20);
+            this.txtExamen1.Name = "txtExamen1";
+            this.txtExamen1.Size = new System.Drawing.Size(100, 26);
+            this.txtExamen1.TabIndex = 12;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 20);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Examen1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(353, 450);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtPromedio);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtExamen3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtExamen2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtExamen1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Promedio";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.TextBox txtPromedio;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtExamen3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtExamen2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtExamen1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ PortafolioSofiaFlores/practica1/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace practica1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            double Examen1 = 0;
+            double Examen2 = 0;
+            double Examen3 = 0;
+            double Promedio = 0;
+            Examen1 = Convert.ToDouble(txtExamen1.Text);
+            Examen2 = Convert.ToDouble(txtExamen2.Text);
+            Examen3 = Convert.ToDouble(txtExamen3.Text);
+            Promedio = (Examen1 + Examen2 + Examen3) / 3;
+            txtPromedio.Text = Promedio.ToString("N2");
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            txtExamen1.Clear();
+            txtExamen2.Clear();
+            txtExamen3.Clear();
+            txtPromedio.Clear();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
